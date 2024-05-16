5220dc1aa5122ace905fa5e62153873a9896a3a0
Temba23 <tembasherpa80@gmail.com>
2024-05-16 18:18:19 +0545

    Finished Automation

--- email_app/urls.py
+++ email_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import email_sender
 
 urlpatterns = [
-    path('send_email/', email_sender, name="send-email"),
+    path('send_email/', email_sender.as_view(), name="send-email"),
 ]
--- email_app/views.py
+++ email_app/views.py
@@ -1,29 +1,32 @@
-from django.contrib.auth.decorators import login_required
 import pandas as pd
 from django.core.mail import send_mail
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 
 location = "D:\document\email_sample.csv"
 
 
-# @login_required
-def email_sender(request):
-    email_from = request.user
-    df = pd.read_csv(location)
 
-    for index, row in df.iterrows():
-        try:
-            email = row['email']
-            subject = row['subject']
-            message = row['message']
-            send_mail(subject, message, email_from, email)
-        except KeyError as e:
-            return Response("Missing expected column in CSV file: %s", e)
-        except Exception as e:
-            return Response("Error sending email to %s: %s", email, e)
+class email_sender(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        email_from = request.user.email
+        df = pd.read_csv(location)
 
-    return Response("Emails have been sent successfully.")
+        for index, row in df.iterrows():
+            try:
+                email = row['email']
+                subject = row['subject']
+                message = row['message']
+                send_mail(subject, message, email_from, [email])
+            except KeyError as e:
+                return Response("Missing expected column in CSV file: %s", e)
+            except Exception as e:
+                return Response("Error sending email to %s: %s", email, e)
+
+        return Response("Emails have been sent successfully.")
 
 
